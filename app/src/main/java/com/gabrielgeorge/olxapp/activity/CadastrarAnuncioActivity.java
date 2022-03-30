c5f3ecb41a27abbaf4ca0ae5b1c46520c66c7a14
Gabriel <biiel.gg22@gmail.com>
2022-03-30 12:12:54 -0300

    Aula 363

--- app/src/main/java/com/gabrielgeorge/olxapp/activity/CadastrarAnuncioActivity.java
+++ app/src/main/java/com/gabrielgeorge/olxapp/activity/CadastrarAnuncioActivity.java
@@ -2,14 +2,38 @@ package com.gabrielgeorge.olxapp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
+import com.blackcat.currencyedittext.CurrencyEditText;
 import com.gabrielgeorge.olxapp.R;
 
+import java.util.Locale;
+
 public class CadastrarAnuncioActivity extends AppCompatActivity {
+
+    private EditText campoTitulo, campoDescricao;
+    private CurrencyEditText campoValor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar_anuncio);
+
+        inicializarComponentes();
+    }
+
+    public void salvarAnuncio(View view){
+        String valor = campoValor.getText().toString();
+    }
+
+    private void inicializarComponentes(){
+        campoTitulo     = findViewById(R.id.editTitulo);
+        campoDescricao  = findViewById(R.id.editDescricao);
+        campoValor      = findViewById(R.id.editValor);
+
+        //Configura a localidade para Brasil
+        Locale locale = new Locale("pt", "BR");
+        campoValor.setLocale(locale);
     }
 }
