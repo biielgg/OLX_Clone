2bde6fe4ac7acec28b448c6be65ddfd4e0793dcb
Gabriel <biiel.gg22@gmail.com>
2022-03-31 15:09:49 -0300

    Aula 365

--- app/src/main/java/com/gabrielgeorge/olxapp/activity/CadastrarAnuncioActivity.java
+++ app/src/main/java/com/gabrielgeorge/olxapp/activity/CadastrarAnuncioActivity.java
@@ -1,5 +1,10 @@
 package com.gabrielgeorge.olxapp.activity;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,6 +12,7 @@ import android.widget.EditText;
 
 import com.blackcat.currencyedittext.CurrencyEditText;
 import com.gabrielgeorge.olxapp.R;
+import com.gabrielgeorge.olxapp.helper.Permissoes;
 import com.santalu.maskedittext.MaskEditText;
 
 import java.util.Locale;
@@ -17,10 +23,17 @@ public class CadastrarAnuncioActivity extends AppCompatActivity {
     private CurrencyEditText campoValor;
     private MaskEditText campoTelefone;
 
+    private String[] permissoes = new String[]{
+            Manifest.permission.READ_EXTERNAL_STORAGE
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar_anuncio);
+
+        //Validar permissões
+        Permissoes.validarPermissoes(permissoes, this, 1);
 
         inicializarComponentes();
     }
@@ -33,10 +46,36 @@ public class CadastrarAnuncioActivity extends AppCompatActivity {
         campoTitulo     = findViewById(R.id.editTitulo);
         campoDescricao  = findViewById(R.id.editDescricao);
         campoValor      = findViewById(R.id.editValor);
-        campoTelefone = findViewById(R.id.editTelefone);
+        campoTelefone   = findViewById(R.id.editTelefone);
 
         //Configura a localidade para Brasil
         Locale locale = new Locale("pt", "BR");
         campoValor.setLocale(locale);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        for(int permissaoResultado : grantResults){
+            if(permissaoResultado == PackageManager.PERMISSION_DENIED){
+                alertaValidacaopermissao();
+            }
+        }
+    }
+
+    private void alertaValidacaopermissao(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Permissões Negadas");
+        builder.setMessage("Para utilizar o programa é necessário aceitar as permissões.");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
